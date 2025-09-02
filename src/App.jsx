7a1ab3101b4acb591a90a9010790a08f7e19e65d
Ajibola Bakare (aka Bolaji) <ajibolabakare@gmail.com>
2025-09-02 19:58:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,10 @@ export const Routers = createBrowserRouter(
       {/* Error page Route if None of the above page is Not found */}
       <Route path='*' element={<NotFound />} />
     </Route>
-  )
+  ),
+  {
+    basename: "/MovieApi", // 👈 Add this line
+  }
 );
 
 function App() {
